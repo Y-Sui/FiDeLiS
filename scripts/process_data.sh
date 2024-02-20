55788db70929fa2f27bf56a52e930676c3c76a85
ysui <yuansui08@gmail.com>
2024-02-20 18:12:58 +0800

    v2 data preprocessing

--- scripts/process_data.sh
+++ scripts/process_data.sh
@@ -1,7 +1,7 @@
 
 # pre-process the dataset
-for dataset in RoG-cwq; do
+for dataset in RoG-cwq RoG-webqsp; do
     for split in train; do
-        python src/align_kg/build_align_qa_dataset.py -d ${dataset} --split ${split}
+        python src/align_kg/build_align_mcq_qa_dataset.py -d ${dataset} --split ${split} --sample 100
     done
 done
