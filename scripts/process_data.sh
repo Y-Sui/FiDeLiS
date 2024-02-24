bde9f58c3bf0d68d1a5c28ccfa565e1fa612a65c
ysui <yuansui08@gmail.com>
2024-02-24 14:44:27 +0800

    update graph_utils

--- scripts/process_data.sh
+++ scripts/process_data.sh
@@ -1,12 +1,12 @@
 
-# pre-process the dataset
-for dataset in RoG-cwq RoG-webqsp; do
-    for split in train; do
-        python src/align_kg/build_align_mcq_qa_dataset.py -d ${dataset} --split ${split} --sample -1
-        python src/joint_training/preprocess_align_mcq.py -d ${dataset} --sample -1
-    done
-done
+# # pre-process the dataset
+# for dataset in RoG-cwq RoG-webqsp; do
+#     for split in train; do
+#         python src/align_kg/build_align_mcq_qa_dataset.py -d ${dataset} --split ${split} --sample 10
+#         python src/joint_training/preprocess_align_mcq.py -d ${dataset} --sample 10
+#     done
+# done
 
-# python src/joint_training/preprocess_align_mcq.py -d RoG-webqsp --sample 2826
+python src/joint_training/preprocess_align_mcq.py -d RoG-webqsp --sample 10
 
-# python src/joint_training/preprocess_align_mcq.py -d RoG-cwq --sample 5200
+python src/joint_training/preprocess_align_mcq.py -d RoG-cwq --sample 10
