a25f079a6572b6f3cdb13d5c48be657cfd80f60a
yuansui <yuansui08@gmail.com>
2024-02-27 23:58:41 +0800

    Update .gitignore and gpt_inference.sh***

--- scripts/gpt_inference.sh
+++ scripts/gpt_inference.sh
@@ -1,6 +1,9 @@
 SOURCE_PATH="/data/shared/yuansui/rog"
 export HF_HOME=${SOURCE_PATH}/.cache/huggingface
 
-for dataset in "RoG-webqsp" "RoG-cwq"; do
-   python gpt_mcq_sandbox.py --sample 10 --d ${dataset} --model_name "gpt-3.5-turbo-0125" --whether_filtering true
+model_name="gpt-4-0125-preview"
+model_name="gpt-3.5-turbo-0125"
+
+for dataset in "RoG-webqsp"; do
+   python gpt_mcq_sandbox.py --sample 3 --d ${dataset} --model_name "gpt-3.5-turbo-0125" --whether_filtering true
 done
