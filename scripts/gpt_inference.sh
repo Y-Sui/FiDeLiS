9f56537b505c11c1d778b294b08a16dbf9cdf311
yuansui <yuansui08@gmail.com>
2024-03-06 14:06:26 +0800

    Update gpt_inference.sh and graph_utils.py

--- scripts/gpt_inference.sh
+++ scripts/gpt_inference.sh
@@ -5,5 +5,5 @@ model_name="gpt-4-0125-preview"
 model_name="gpt-3.5-turbo-0125"
 
 for dataset in "RoG-webqsp"; do
-   python gpt_mcq_sandbox.py --sample 3 --d ${dataset} --model_name "gpt-3.5-turbo-0125" --whether_filtering true
+   python gpt_mcq_sandbox.py --sample 3 --d ${dataset} --model_name "gpt-3.5-turbo-0125" --retrieval_type "vector_rag"
 done
