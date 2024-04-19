68db2ae5e1cfbdcae365b39cb2e8dd542774f2d0
yuansui <yuansui08@gmail.com>
2024-04-19 23:58:42 +0800

    Remove Unused Files, Add new dataset

--- scripts/gpt_inference.sh
+++ scripts/gpt_inference.sh
@@ -2,13 +2,16 @@ SOURCE_PATH="/data/shared/yuansui/rog"
 export HF_HOME=${SOURCE_PATH}/.cache/huggingface
 
 # MODEL_NAME="gpt-4-0125-preview"
-# MODEL_NAME="gpt-3.5-turbo-0125"
+MODEL_NAME="gpt-3.5-turbo-0125"
+EMBEDDING_MODEL="text-embedding-3-small"
 DATASET_LIST="RoG-webqsp RoG-cwq"
-# DATASET_LIST="RoG-webqsp"
-RETRIEVAL_TYPE="vector_rag"
 
+# Generate Embeddings
 for DATA_NAME in $DATASET_LIST; do
    python run.py \
       --sample -1 \
-      --d ${DATA_NAME} 
+      --d ${DATA_NAME} \
+      --model_name ${MODEL_NAME} \
+      --embedding_model ${EMBEDDING_MODEL} \
+      --generate_embeddings True
 done
